9887362f32cea8332df734663f963912ac5e32c0
Ignacio Espósito <nachoesposito@gmail.com>
2026-02-23 23:52:03 +0100

    build: added expiration date for the token

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const API = "/api";
 
 function saveToken(token) {
-  localStorage.setItem("token", token);
+  const now = new Date()
+  const item = {
+		token: token,
+		expiry: now.getTime() + 1800000,
+	}
+  localStorage.setItem("token", JSON.stringify(item));
 }
 
 function getToken() {
-  return localStorage.getItem("token");
+  const itemStr = localStorage.getItem("token");
+  const item = JSON.parse(itemStr)
+	const now = new Date()
+  if (now.getTime() > item.expiry) {
+		localStorage.removeItem("token")
+		return null
+	}
+  return item.value();
 }
